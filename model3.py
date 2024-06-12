5c5a0fc355255ebf2346cfece5cbaa25f74ac8a9
set5u <ajiken4610@gmail.com>
2024-06-12 20:47:20 +0900

    save

--- model3.py
+++ model3.py
@@ -32,12 +32,12 @@ def load(weights: str):
 
 
 class AddNorm(tf.keras.Model):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
         self.norm = tf.keras.layers.LayerNormalization()
 
     def build(self, input_shape):
-        self.norm.build(input_shape[0])
+        self.norm.build(input_shape)
 
     def call(self, *inputs):
         return self.norm(inputs[0] + inputs[1])
@@ -55,11 +55,11 @@ class FF(tf.keras.Model):
         self.maxLen = maxLen
 
     def build(self, input_shape):
-        self.ff0.build(input_shape[0])
-        self.ff1.build(input_shape[0])
+        self.ff0.build(input_shape)
+        self.ff1.build(self.ff0.compute_output_shape(input_shape))
 
-    def call(self, *inputs):
-        ret = self.ff0(inputs[0])
+    def call(self, input):
+        ret = self.ff0(input)
         ret = self.ff1(ret)
         return ret
 
@@ -67,26 +67,91 @@ class FF(tf.keras.Model):
         return input_shape[0][0:1] + (self.maxLen, self.dModel)
 
 
+class Reducer(tf.keras.Model):
+    def call(self, input):
+        return tf.reduce_sum(input, 2)
+
+
 def useBERTLarge(
     depthInput,
     depthOutput,
-    embeddingD=1024,
-    dModel=1024,
-    dFF=4096,
+    embeddingD=512,
+    dModel=512,
+    dFF=2048,
     h=16,
     maxLen=8,
     layers=24,
 ):
-    pass
+    input = tf.keras.layers.Input((None, maxLen))
+    embedding = tf.keras.layers.Embedding(depthInput, embeddingD)(input)
+    bypass = []
+    lastOutput = embedding
+    for i in range(layers):
+        multiHeadAttention = tf.keras.layers.MultiHeadAttention(h, dModel)(
+            lastOutput, lastOutput
+        )
+        addNorm0 = AddNorm()(multiHeadAttention, lastOutput)
+        ff = tf.keras.layers.TimeDistributed(FF(dModel, dFF, maxLen))(addNorm0)
+        addNorm1 = AddNorm()(addNorm0, ff)
+        bypass.append(addNorm1)
+        lastOutput = addNorm1
+        j = 1
+        while (i + 1) % j == 0:
+            lastOutput = AddNorm()(bypass[i - j + 1], lastOutput)
+            j *= 2
+    attn = tf.keras.layers.MultiHeadAttention(h, dModel)(lastOutput, lastOutput)
+    reducer = Reducer()(attn)
+    gru = tf.keras.layers.GRU(dModel)(reducer)
+    dense = tf.keras.layers.Dense(depthOutput, activation="softmax")(gru)
+    model = tf.keras.Model(input, dense)
+    optimizer = tf.keras.optimizers.Adadelta(1.0)
+    model.compile(
+        optimizer,
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+    return model
+
+
+with open("./num2word.json") as f:
+    num2word = json.loads("".join(f.readlines()))
+with open("./word2num.json") as f:
+    word2num = json.loads("".join(f.readlines()))
+with open("./wordTokens.json") as f:
+    tokens = json.loads("".join(f.readlines()))
+depth = len(num2word)
+# large = useBERTLarge(depth, depth)
+# large.summary()
+# tf.keras.utils.plot_model(large, "large.png", show_shapes=True)
+
+batchSize = 4
+stepsPerEpoch = 4
+
+
+def loader():
+    while True:
+        input = []
+        output = []
+        for _ in range(batchSize * stepsPerEpoch):
+            startIndex = math.floor(random.random() * (len(tokens) - 257))
+            input.append(tokens[startIndex : startIndex + 256])
+            endIndex = startIndex + 256
+            while tokens[endIndex] == 3:
+                endIndex += 1
+            output.append(tokens[endIndex])
+        yield np.array(input).reshape((batchSize, -1, 8)), np.array(output)
+
+
+data = loader()
 
 
 def useBERTTeacher(
     depthInput,
     depthOutput,
     embeddingD=128,
-    dModelInter=512,
-    dModelIntra=1024,
-    dFF=4096,
+    dModelInter=256,
+    dModelIntra=512,
+    dFF=1024,
     h=4,
     maxLen=8,
     layers=24,
@@ -98,11 +163,11 @@ def useBERTStudent(
     depthInput,
     depthOutput,
     embeddingD=128,
-    dModelInter=512,
+    dModelInter=256,
     dModelIntra=128,
     dFF=512,
     h=4,
-    r=4,
+    r=2,
     maxLen=8,
     layers=24,
 ):
