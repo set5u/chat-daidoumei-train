459d4b1d8f55405b908dfa1eafd96ac11e0aba40
set5u <ajiken4610@gmail.com>
2024-06-12 14:07:13 +0900

    save

--- model3.py
+++ model3.py
@@ -67,24 +67,6 @@ class FF(tf.keras.Model):
         return input_shape[0][0:1] + (self.maxLen, self.dModel)
 
 
-class AttentionRNNCell(tf.keras.Model):
-    def __init__(self, dModel, h, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.attn = tf.keras.layers.MultiHeadAttention(h, dModel)
-        self.addNorm = AddNorm()
-
-    def build(self, input_shape):
-        self.attn.build(input_shape[0:1] + (1,) + input_shape[1:2])
-
-    def call(self, input):
-        ret = self.attn(
-            tf.reshape(input[1], (input.shape[0], 1, input.shape[1])),
-            tf.reshape(input[0], (input.shape[0], 1, input.shape[1])),
-        )
-        ret = self.addNorm(ret, input[1])
-        return [ret, ret]
-
-
 def useBERTLarge(dModel, dFF, h, depthInput, depthOutput, layers):
     pass
 
